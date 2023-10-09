8e3c17e7c650927af3feb808045993516c360592
sunflower456 <ghkdalki456@naver.com>
2023-10-09 15:29:56 +0900

    Update from_request_to_telegraf_exec_plugin.sh

--- from_request_to_telegraf_exec_plugin.sh
+++ from_request_to_telegraf_exec_plugin.sh
@@ -2,17 +2,19 @@
 
 for k in  $(echo $(kubectl get pods -l app=frontend -n sunhwa -o jsonpath="{.items[*]}") | jq '[.metadata.name, .spec.containers[0].resources.requests]' | jq -s '.'| jq -c '.[]'); do
         name=$(echo ${k} | jq '.[0]');
-	cpu=$(echo ${k} | jq '.[1].cpu');
-	
+        cpu=$(echo ${k} | jq '.[1].cpu');
         if [[ ${cpu} == *"m"* ]]; then
                 cpuCore=$(echo $cpu | sed -e "s/m//g")
                 cpuCore=$(echo $cpuCore | sed 's/"//g')
                 cpuNum=$(echo $((cpuCore)))
                 core=1000
                 cpuresult=$(bc -l <<< $((cpuNum))/$((core)))
+        else
+                cpu=$(echo ${cpu} | sed 's/"//g')
+                cpuresult=$((cpu))
 
         fi
-        echo 'vpa_pod_request,pod='$name',resource=cpu gauge='$cpuresult ;
+        echo 'vpa_pod_request,pod='$name',resource="cpu" gauge='$cpuresult;
         memory=$(echo ${k} | jq '.[1].memory');
 	if [[ ${memory} == *"k"* ]]; then
 	        mem=$(echo $memory | sed -e "s/k//g")
@@ -20,8 +22,11 @@ for k in  $(echo $(kubectl get pods -l app=frontend -n sunhwa -o jsonpath="{.ite
                 memBytes=$(echo $((mem)))
                 bytes=1000
                 memresult=$(bc -l <<< $((memBytes))*$((bytes)))
+        else
+                memory=$(echo ${memory} | sed 's/"//g')
+                memresult=$((memory))
 	fi
-        echo 'vpa_pod_request,pod='$name',resource=memory gauge='$memresult ;
+        echo 'vpa_pod_request,pod='$name',resource="memory" gauge='$memresult;
 done
 name='frontend-vpa';
 
@@ -49,7 +54,7 @@ else
   lowerBoundCpu=$(echo ${lowerBoundCpu} | sed 's/"//g')
   lowerBoundCpu=$((lowerBoundCpu))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=lowerBound,unit=cores gauge='${lowerBoundCpu} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=lowerBound,unit=cores gauge='${lowerBoundCpu};
 
 targetCpu=$(echo $(kubectl get vpa frontend-vpa -n sunhwa -o jsonpath="{.status.recommendation.containerRecommendations}") | jq '.[0].target.cpu');
 if [[ ${targetCpu} == *"m"* ]]; then
@@ -62,7 +67,7 @@ else
   targetCpu=$(echo ${targetCpu} | sed 's/"//g')
   targetCpu=$((targetCpu))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=target,unit=cores gauge='${targetCpu} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=target,unit=cores gauge='${targetCpu};
 
 uncappedTargetCpu=$(echo $(kubectl get vpa frontend-vpa  -n sunhwa -o jsonpath="{.status.recommendation.containerRecommendations}")  | jq '.[0].uncappedTarget.cpu' );
 if [[ ${uncappedTargetCpu} == *"m"* ]]; then
@@ -75,7 +80,7 @@ else
   uncappedTargetCpu=$(echo ${uncappedTargetCpu} | sed 's/"//g')
   uncappedTargetCpu=$((uncappedTargetCpu))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=uncappedTarget,unit=cores gauge='${uncappedTargetCpu} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=uncappedTarget,unit=cores gauge='${uncappedTargetCpu};
 
 
 upperBoundCpu=$(echo $(kubectl get vpa frontend-vpa  -n sunhwa -o jsonpath="{.status.recommendation.containerRecommendations}") | jq '.[0].upperBound.cpu');
@@ -89,7 +94,7 @@ else
   upperBoundCpu=$(echo ${upperBoundCpu} | sed 's/"//g')
   upperBoundCpu=$((upperBoundCpu))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=upperBound,unit=cores gauge='${upperBoundCpu} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=cpu,recommendation=upperBound,unit=cores gauge='${upperBoundCpu};
 
 
 upperBoundMemory=$(echo $(kubectl get vpa frontend-vpa -n sunhwa -o jsonpath="{.status.recommendation.containerRecommendations}")  | jq '.[0].upperBound.memory');
@@ -103,7 +108,7 @@ else
   upperBoundMemory=$(echo ${upperBoundMemory} | sed 's/"//g')
   upperBoundMemory=$((upperBoundMemory))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=upperBound,unit=bytes gauge='${upperBoundMemory} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=upperBound,unit=bytes gauge='${upperBoundMemory};
 
 targetMemory=$(echo $(kubectl get vpa frontend-vpa  -n sunhwa -o jsonpath="{.status.recommendation.containerRecommendations}") | jq '.[0].target.memory');
 if [[ ${targetMemory} == *"k"* ]]; then
@@ -116,7 +121,7 @@ else
   targetMemory=$(echo ${targetMemory} | sed 's/"//g')
   targetMemory=$((targetMemory))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=target,unit=bytes gauge='${targetMemory} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=target,unit=bytes gauge='${targetMemory};
 
 
 lowerBoundMemory=$(echo $(kubectl get vpa frontend-vpa -n sunhwa -o jsonpath="{.status.recommendation.containerRecommendations}")  | jq '.[0].lowerBound.memory');
@@ -130,7 +135,7 @@ else
   lowerBoundMemory=$(echo ${lowerBoundMemory} | sed 's/"//g')
   lowerBoundMemory=$((lowerBoundMemory))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=lowerBound,unit=bytes gauge='${lowerBoundMemory} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=lowerBound,unit=bytes gauge='${lowerBoundMemory};
 
 uncappedTargetMemory=$(echo $(kubectl get vpa frontend-vpa -n sunhwa -o jsonpath="{.status.recommendation.containerRecommendations}")  | jq '.[0].uncappedTarget.memory');
 if [[ ${uncappedTargetMemory} == *"k"* ]]; then
@@ -143,4 +148,4 @@ else
   uncappedTargetMemory=$(echo ${uncappedTargetMemory} | sed 's/"//g')
   uncappedTargetMemory=$((uncappedTargetMemory))
 fi
-echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=uncappedTarget,unit=bytes gauge='${uncappedTargetMemory} ;
+echo 'vpa_target_info,vpa_name='${name}',resource=memory,recommendation=uncappedTarget,unit=bytes gauge='${uncappedTargetMemory};
